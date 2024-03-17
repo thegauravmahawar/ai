0001d12acf234bab236e029cccf156a83c57819f
Gaurav Mahawar <thegauravmahawar@gmail.com>
2024-03-17 23:17:46 +0530

    Prompt basics

--- chatgpt-prompt-engineering/README.md
+++ chatgpt-prompt-engineering/README.md
@@ -6,19 +6,32 @@
 
 ## Introduction
 
-There are two types of LLMs (Large Language Models)
+There are two types of LLMs (Large Language Models):
 
-| Base LLM                                                                                                                | Instruction Tuned LLM                                                                                        |
-| ----------------------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------ |
+| Base LLM                                                                                                                | Instruction Tuned LLM         |
+| ----------------------------------------------------------------------------------------------------------------------- | ----------------------------- |
 | Predicts next word, based on text training data.                                                                        | Tries to follow instructions. |
-| :white_check_mark: _Once upon a time, there was a unicorn_ that lived in a magical forest with all her unicorn friends | |
-| :x: _What is the capital of France?_   |   |
+| :white*check_mark: \_Once upon a time, there was a unicorn* that lived in a magical forest with all her unicorn friends |                               |
+| :x: _What is the capital of France?_                                                                                    |                               |
 
 Instruction Tuned LLMs use RLHF (Reinforcement Learning with Human Feedback) to make the system better able to learn and follow instructions.
 
 We can tune the output by giving specific prompts and defining the tone of those prompts.
 
 ## Guidelines
+
+Prompting Principles:
+
+- Write clear and specific instructions
+  - Use delimiters to clearly indicate distinct parts of the input (``, "", <>, :)
+  - Ask for a structured output - JSON, HTML
+  - Ask the model to check whether conditions are satisfied
+  - "Few-shot" prompting
+- Give the model time to "think"
+  - Specify the steps required to complete a task
+  - Ask for the output in a specified format
+
+Tactics:
 
 ## Iterative
 
--- chatgpt-prompt-engineering/prompt.py
+++ chatgpt-prompt-engineering/prompt.py
@@ -17,3 +17,133 @@ def get_completion(prompt, model='gpt-3.5-turbo'):
     temperature = 0, # this is the degree of randomness of the model's output
   )
   return response.choices[0].message.content
+
+# Use delimiters
+text1 = f"""
+You should express what you want a model to do by \ 
+providing instructions that are as clear and \ 
+specific as you can possibly make them. \ 
+This will guide the model towards the desired output, \ 
+and reduce the chances of receiving irrelevant \ 
+or incorrect responses. Don't confuse writing a \ 
+clear prompt with writing a short prompt. \ 
+In many cases, longer prompts provide more clarity \ 
+and context for the model, which can lead to \ 
+more detailed and relevant outputs.
+"""
+
+prompt1 = f"""
+Summarize the text delimited by triple backticks \ 
+into a single sentence.
+```{text1}```
+"""
+
+# Ask for a structured output
+prompt2 = f"""
+Generate a list of three made-up book titles along \ 
+with their authors and genres. 
+Provide them in JSON format with the following keys: 
+book_id, title, author, genre.
+"""
+
+# Ask the model to check whether conditions are satisfied
+text3 = f"""
+Making a cup of tea is easy! First, you need to get some \ 
+water boiling. While that's happening, \ 
+grab a cup and put a tea bag in it. Once the water is \ 
+hot enough, just pour it over the tea bag. \ 
+Let it sit for a bit so the tea can steep. After a \ 
+few minutes, take out the tea bag. If you \ 
+like, you can add some sugar or milk to taste. \ 
+And that's it! You've got yourself a delicious \ 
+cup of tea to enjoy.
+"""
+
+prompt3 = f"""
+You will be provided with text delimited by triple quotes. 
+If it contains a sequence of instructions, \ 
+re-write those instructions in the following format:
+
+Step 1 - ...
+Step 2 - …
+…
+Step N - …
+
+If the text does not contain a sequence of instructions, \ 
+then simply write \"No steps provided.\"
+
+\"\"\"{text3}\"\"\"
+"""
+
+text4 = f"""
+The sun is shining brightly today, and the birds are \
+singing. It's a beautiful day to go for a \ 
+walk in the park. The flowers are blooming, and the \ 
+trees are swaying gently in the breeze. People \ 
+are out and about, enjoying the lovely weather. \ 
+Some are having picnics, while others are playing \ 
+games or simply relaxing on the grass. It's a \ 
+perfect day to spend time outdoors and appreciate the \ 
+beauty of nature.
+"""
+
+prompt4 = f"""
+You will be provided with text delimited by triple quotes. 
+If it contains a sequence of instructions, \ 
+re-write those instructions in the following format:
+
+Step 1 - ...
+Step 2 - …
+…
+Step N - …
+
+If the text does not contain a sequence of instructions, \ 
+then simply write \"No steps provided.\"
+
+\"\"\"{text4}\"\"\"
+"""
+
+# "Few-shot" prompting
+prompt5 = f"""
+Your task is to answer in a consistent style.
+
+<child>: Teach me about patience.
+
+<grandparent>: The river that carves the deepest \ 
+valley flows from a modest spring; the \ 
+grandest symphony originates from a single note; \ 
+the most intricate tapestry begins with a solitary thread.
+
+<child>: Teach me about resilience.
+"""
+
+# Specify the steps required to complete a task
+text6 = f"""
+In a charming village, siblings Jack and Jill set out on \ 
+a quest to fetch water from a hilltop \ 
+well. As they climbed, singing joyfully, misfortune \ 
+struck—Jack tripped on a stone and tumbled \ 
+down the hill, with Jill following suit. \ 
+Though slightly battered, the pair returned home to \ 
+comforting embraces. Despite the mishap, \ 
+their adventurous spirits remained undimmed, and they \ 
+continued exploring with delight.
+"""
+
+prompt6 = f"""
+Perform the following actions: 
+1 - Summarize the following text delimited by triple \
+backticks with 1 sentence.
+2 - Translate the summary into French.
+3 - List each name in the French summary.
+4 - Output a json object that contains the following \
+keys: french_summary, num_names.
+
+Separate your answers with line breaks.
+
+Text:
+```{text6}```
+"""
+
+response = get_completion(prompt6)
+print(response)
